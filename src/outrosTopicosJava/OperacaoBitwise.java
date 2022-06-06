bd4275f10f3e0ced0cf1705b600f43725f05ee1d
Daniel da silva pedro <danielsp699@outlook.com>
2022-06-06 19:34:33 -0300

    BitWise Operation in Java

---
+++ src/outrosTopicosJava/OperacaoBitwise.java
@@ -0,0 +1,13 @@
+package outrosTopicosJava;
+
+public class OperacaoBitwise {
+    public static void main(String[] args) {
+
+        // Comparando o binário dos números abaixo
+        int n1 = 89;
+        int n2 = 60;
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+    }
+}
