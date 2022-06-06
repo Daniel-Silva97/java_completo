874e5d9a21e9315c3ab7907b99c1284a35611301
Daniel da silva pedro <danielsp699@outlook.com>
2022-06-06 20:25:08 -0300

    Problem solved without POO.

---
+++ src/introducaoOrientadaObjeto/naoOrientadaObjeto/application/problemaSemOrientadoObjeto.java
@@ -0,0 +1,42 @@
+package introducaoOrientadaObjeto.naoOrientadaObjeto.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problemaSemOrientadoObjeto {
+    public static void main(String[] args) {
+        // Calculando a área do Triângulo sem utilizar Linguagem Orientada a objetos
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double xA, xB, xC, yA, yB, yC;
+
+
+        System.out.println("Enter the measures of triangule X: ");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+
+        System.out.println("Enter the measures of triangule Y: ");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double pX = (xA + xB + xC) / 2;
+        double areaX = Math.sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+
+        double pY = (yA + yB + yC) / 2;
+        double areaY = Math.sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+
+        sc.close();
+    }
+}
