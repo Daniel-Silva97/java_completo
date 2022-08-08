a91282b98830e65d50a7c2568eeecbfdac70a680
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-08 19:12:12 -0300

    Try with resources

--- src/trabalhandoComArquivos/fileBufferedReader/application/Program.java
+++ src/trabalhandoComArquivos/fileBufferedReader/application/Program.java
@@ -7,14 +7,9 @@ import java.io.IOException;
 public class Program {
     public static void main(String[] args) {
         String path = "C:\\Temp\\teste.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -23,18 +18,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
         }
     }
 }
