adb0b8a7a5f30fc945b3afc4add7c8bb8ea2d9b5
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-22 19:37:46 -0300

    Comparator - Method with LAMBDA

--- src/programacaoFuncionalExpressoesLambda/comparator/application/Program.java
+++ src/programacaoFuncionalExpressoesLambda/comparator/application/Program.java
@@ -4,6 +4,7 @@ package programacaoFuncionalExpressoesLambda.comparator.application;
 import programacaoFuncionalExpressoesLambda.comparator.entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -14,7 +15,7 @@ public class Program {
         products.add(new Product("Notebook", 1200.0));
         products.add(new Product("Tablet", 400.0));
 
-        products.sort(new MyComparator());
+        products.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product product : products) {
             System.out.println(product);
