bc65c857fa36df7a3174acd517bdab9bb913ff85
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-23 09:47:41 -0300

    Predicate - Class util

---
+++ src/programacaoFuncionalExpressoesLambda/predicate/util/ProductPredicate.java
@@ -0,0 +1,10 @@
+package programacaoFuncionalExpressoesLambda.predicate.util;
+import programacaoFuncionalExpressoesLambda.predicate.entities.Product;
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100;
+    }
+}
