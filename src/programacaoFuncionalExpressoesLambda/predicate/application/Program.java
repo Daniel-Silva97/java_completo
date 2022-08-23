0e10a9fa107a40abbefb2df44a7b42328e77b0a0
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-23 09:53:16 -0300

    Predicate - LAMBDA main program

--- src/programacaoFuncionalExpressoesLambda/predicate/application/Program.java
+++ src/programacaoFuncionalExpressoesLambda/predicate/application/Program.java
@@ -1,11 +1,11 @@
 package programacaoFuncionalExpressoesLambda.predicate.application;
 
 import programacaoFuncionalExpressoesLambda.predicate.entities.Product;
-import programacaoFuncionalExpressoesLambda.predicate.util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -17,7 +17,9 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= 100;
+
+        list.removeIf(pred);
 
         for (Product p : list) {
             System.out.println(p);
