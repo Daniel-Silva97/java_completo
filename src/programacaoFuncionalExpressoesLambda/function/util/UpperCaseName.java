74ec7b8a6a095b00c970f61160ae9136f412dcaf
Daniel da silva pedro <danielsp699@outlook.com>
2022-08-23 10:17:37 -0300

    Function - Class util (map function)

---
+++ src/programacaoFuncionalExpressoesLambda/function/util/UpperCaseName.java
@@ -0,0 +1,12 @@
+package programacaoFuncionalExpressoesLambda.function.util;
+
+import programacaoFuncionalExpressoesLambda.function.entities.Product;
+
+import java.util.function.Function;
+
+public class UpperCaseName implements Function<Product, String> {
+    @Override
+    public String apply(Product product) {
+        return product.getName().toUpperCase();
+    }
+}
